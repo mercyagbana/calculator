1f1c38c236ab310ff4c4eef5a80a266f39828ca6
mercyagbana <agbanamercy067@gmail.com>
2025-07-08 11:23:59 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// CALCULATOR PROGRAM
+
+const display = document.getElementById ("display");
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculate () {
+    try {
+       display.value = eval(display.value);
+
+    } catch(e) {
+        console.error(e);
+        display.value = "Error";
+    }
+}
